2ac10223c5273833b8d31d792f835abb2bdcfb3e
Amaury Clain <amauryclain@gmail.com>
2024-01-06 15:16:41 +0100

    nouveau mcd

--- mcdMatchingSql.sql
+++ mcdMatchingSql.sql
@@ -11,7 +11,6 @@ CREATE TABLE groupe(
         grp_id         Int  Auto_increment  NOT NULL ,
         grp_name       Varchar (200) NOT NULL ,
         grp_createdate Date ,
-        grp_genre      Varchar (150) NOT NULL ,
         grp_img        Text
 	,CONSTRAINT groupe_PK PRIMARY KEY (grp_id)
 )ENGINE=InnoDB;
@@ -26,34 +25,63 @@ CREATE TABLE membre(
         mbr_firstName Varchar (100) NOT NULL ,
         mbr_lastName  Varchar (100) NOT NULL ,
         mbr_role      Varchar (70) NOT NULL ,
-        mbr_birthdate Date NOT NULL
+        mbr_birthdate Date NOT NULL ,
+        mbr_joinDate  Date NOT NULL ,
+        mbr_nickname  Varchar (100)
 	,CONSTRAINT membre_PK PRIMARY KEY (mbr_id)
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: joindate
+# Table: music_genre
 #------------------------------------------------------------
 
-CREATE TABLE joindate(
-        jdt_id   Int  Auto_increment  NOT NULL ,
-        jdt_date Date NOT NULL
-	,CONSTRAINT joindate_PK PRIMARY KEY (jdt_id)
+CREATE TABLE music_genre(
+        mgr_id   Int  Auto_increment  NOT NULL ,
+        mgr_name Varchar (200) NOT NULL
+	,CONSTRAINT music_genre_PK PRIMARY KEY (mgr_id)
 )ENGINE=InnoDB;
 
 
 #------------------------------------------------------------
-# Table: appartenir
+# Table: album
 #------------------------------------------------------------
 
-CREATE TABLE appartenir(
-        mbr_id Int NOT NULL ,
-        grp_id Int NOT NULL ,
-        jdt_id Int NOT NULL
-	,CONSTRAINT appartenir_PK PRIMARY KEY (mbr_id,grp_id,jdt_id)
+CREATE TABLE album(
+        alb_id         Int  Auto_increment  NOT NULL ,
+        alb_name       Varchar (300) NOT NULL ,
+        alb_createdate Date NOT NULL ,
+        grp_id         Int NOT NULL
+	,CONSTRAINT album_PK PRIMARY KEY (alb_id)
 
-	,CONSTRAINT appartenir_membre_FK FOREIGN KEY (mbr_id) REFERENCES membre(mbr_id)
-	,CONSTRAINT appartenir_groupe0_FK FOREIGN KEY (grp_id) REFERENCES groupe(grp_id)
-	,CONSTRAINT appartenir_joindate1_FK FOREIGN KEY (jdt_id) REFERENCES joindate(jdt_id)
+	,CONSTRAINT album_groupe_FK FOREIGN KEY (grp_id) REFERENCES groupe(grp_id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: grp_membre
+#------------------------------------------------------------
+
+CREATE TABLE grp_membre(
+        mbr_id Int NOT NULL ,
+        grp_id Int NOT NULL
+	,CONSTRAINT grp_membre_PK PRIMARY KEY (mbr_id,grp_id)
+
+	,CONSTRAINT grp_membre_membre_FK FOREIGN KEY (mbr_id) REFERENCES membre(mbr_id)
+	,CONSTRAINT grp_membre_groupe0_FK FOREIGN KEY (grp_id) REFERENCES groupe(grp_id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: grp_genre
+#------------------------------------------------------------
+
+CREATE TABLE grp_genre(
+        grp_id Int NOT NULL ,
+        mgr_id Int NOT NULL
+	,CONSTRAINT grp_genre_PK PRIMARY KEY (grp_id,mgr_id)
+
+	,CONSTRAINT grp_genre_groupe_FK FOREIGN KEY (grp_id) REFERENCES groupe(grp_id)
+	,CONSTRAINT grp_genre_music_genre0_FK FOREIGN KEY (mgr_id) REFERENCES music_genre(mgr_id)
 )ENGINE=InnoDB;
 
