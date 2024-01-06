046351e3518b1df53f856ef6a645e22d3ee5e8fb
Amaury Clain <amauryclain@gmail.com>
2024-01-06 16:20:08 +0100

    moved connection

---
+++ connection.php
@@ -0,0 +1,31 @@
+<?php
+try {
+
+    $mysqlClient = new PDO(
+        'mysql:host=localhost:8889;dbname=matching_music_group;charset=utf8',
+        'root',
+        'root',
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+} catch (PDOException $e) {
+    die('Erreur: ' . $e->getMessage());
+}
+// teststststfsdxhdrtgh
+
+$sqlquery = 'select g.grp_name, m.mbr_nickname from groupe g
+inner join grp_membre gm on g.grp_id = gm.grp_id
+inner join membre m on m.mbr_id = gm.mbr_id
+where mbr_nickname = :mbr_nickname and g.grp_id = :grp_id';
+
+$groupeliste = $mysqlClient->prepare($sqlquery);
+$groupeliste->execute([
+    'mbr_nickname' => "Dimebag Darrell",
+    'grp_id' => 1,
+]);
+$groupes = $groupeliste->fetchAll();
+
+foreach ($groupes as $groupe) {
+?>
+    <p><?php echo "Nom groupe : {$groupe['grp_name']} ", "Nom du membre : {$groupe['mbr_nickname']}"; ?></p>
+<?php
+}
