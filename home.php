d472813b8b259e10fabe01c6d77c0a53196ed58f
Amaury Clain <amauryclain@gmail.com>
2024-01-08 19:13:50 +0100

    fin recherche avec criteres

--- home.php
+++ home.php
@@ -30,19 +30,21 @@ if (isset($submit) && !empty(trim($keywords)) && $searchType == "grp") {
 // Recherche musiciens
 if ($searchType == "mem") {
     $displaySearch = "yes";
-};
-if (isset($submit) && !empty(trim($keywords)) && $searchType == "mem") {
-    $words = explode(" ", trim($keywords));
-    for ($i = 0; $i < count($words); $i++) {
-        $kw[$i] = "g.grp_name like '%" . $words[$i] . "%'";
+
+    if (isset($submit) && !empty(trim($keywords))) {
+        $words = explode(" ", trim($keywords));
+        $kw = array_map(function ($word) {
+            return "CONCAT(m.mbr_firstName, ' ', m.mbr_lastName) LIKE '%" . $word . "%'";
+        }, $words);
+
+        include('connection.php');
+        $res = $mysqlClient->prepare('SELECT m.mbr_firstName, m.mbr_lastName, m.mbr_nickname FROM membre m WHERE ' . implode(' OR ', $kw));
+        $res->setFetchMode(PDO::FETCH_ASSOC);
+        $res->execute();
+        $tab = $res->fetchAll();
+        $displayMem = "yes";
     }
-    include('connection.php');
-    $res = $mysqlClient->prepare('select g.grp_name from groupe g where ' . implode(' or ', $kw) . ' order by g.grp_name asc');
-    $res->setFetchMode(PDO::FETCH_ASSOC);
-    $res->execute();
-    $tab = $res->fetchAll();
-    $displayMem = "yes";
-};
+}
 // Recherche albums
 if ($searchType == "alb") {
     $displaySearch = "yes";
@@ -50,10 +52,10 @@ if ($searchType == "alb") {
 if (isset($submit) && !empty(trim($keywords)) && $searchType == "alb") {
     $words = explode(" ", trim($keywords));
     for ($i = 0; $i < count($words); $i++) {
-        $kw[$i] = "g.grp_name like '%" . $words[$i] . "%'";
+        $kw[$i] = "a.alb_name like '%" . $words[$i] . "%'";
     }
     include('connection.php');
-    $res = $mysqlClient->prepare('select g.grp_name from groupe g where ' . implode(' or ', $kw) . ' order by g.grp_name asc');
+    $res = $mysqlClient->prepare('select a.alb_name from album a where ' . implode(' or ', $kw));
     $res->setFetchMode(PDO::FETCH_ASSOC);
     $res->execute();
     $tab = $res->fetchAll();
@@ -103,7 +105,7 @@ if (isset($submit) && !empty(trim($keywords)) && $searchType == "alb") {
             <div class="card" style="width: 18rem;">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title"><?php echo $tab[$i]["grp_name"] ?></h5>
+                    <h5 class="card-title"><?php echo $tab[$i]["mbr_firstName"] . ' ' . $tab[$i]["mbr_lastName"] ?></h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
@@ -120,7 +122,7 @@ if (isset($submit) && !empty(trim($keywords)) && $searchType == "alb") {
             <div class="card" style="width: 18rem;">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title"><?php echo $tab[$i]["grp_name"] ?></h5>
+                    <h5 class="card-title"><?php echo $tab[$i]["alb_name"] ?></h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
